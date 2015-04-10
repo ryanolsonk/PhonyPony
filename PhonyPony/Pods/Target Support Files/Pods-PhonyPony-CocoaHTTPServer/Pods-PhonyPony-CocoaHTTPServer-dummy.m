ad471c30ac7921f7ed32bf92e425166e6f4bdda0
Ryan Olson <ryanolsonk@gmail.com>
2015-04-09 17:41:12 -0700

    Add CocoaHTTPServer pod

---
+++ PhonyPony/Pods/Target Support Files/Pods-PhonyPony-CocoaHTTPServer/Pods-PhonyPony-CocoaHTTPServer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhonyPony_CocoaHTTPServer : NSObject
+@end
+@implementation PodsDummy_Pods_PhonyPony_CocoaHTTPServer
+@end
---
+++ PhonyPony/Pods/Target Support Files/Pods-PhonyPony/Pods-PhonyPony-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhonyPony : NSObject
+@end
+@implementation PodsDummy_Pods_PhonyPony
+@end
