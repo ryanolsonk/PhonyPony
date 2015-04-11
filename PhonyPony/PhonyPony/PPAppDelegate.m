ee8c672d849bf3da555020f41bbd51d255f2aad9
Ryan Olson <ryanolsonk@gmail.com>
2015-04-11 10:14:33 -0700

    Use SVWebViewController to display the Pony Gateway and Dev Tools.

--- PhonyPony/PhonyPony/PPAppDelegate.m
+++ PhonyPony/PhonyPony/PPAppDelegate.m
@@ -8,13 +8,20 @@
 
 #import "PPAppDelegate.h"
 #import "PPGatewayServer.h"
+#import <SVWebViewController/SVWebViewController.h>
 
 @implementation PPAppDelegate
-
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     [[PPGatewayServer sharedGatewayServer] start];
+
+    SVWebViewController *webViewController = [[SVWebViewController alloc] initWithAddress:@"http://localhost:9000"];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:webViewController];
+
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = navigationController;
+    [self.window makeKeyAndVisible];
     
     return YES;
 }
